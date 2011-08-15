abf1161c2457de4c4be4e2444c77d876fe59dc55
D. Elmo Peele <depeele@tycho.ncsc.mil>
2011-08-15 13:08:09 -0400

    Remove 0001.html and related files.  Add direct src reference.

--- samples/0001/redirect.js
+++
@@ -1,3 +0,0 @@
-// File left intentionally blank. This script is only served to mobile devices.
-// If you should be redirected, try clearing your cookies and cache.
-// http://community.mobify.me/guides/mobile-device-detection/
