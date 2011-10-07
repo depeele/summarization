acf8f3f3a6c50bef09321f3ea9086e72314b06ed
D. Elmo Peele <depeele@tycho.ncsc.mil>
2011-10-07 11:19:16 -0400

    Add a click helper and content containers.
    public/js/helper/click.js
        A click helper that can be mixed in with any view.
    
    public/js/view/doc.js
        Begin including rangy for selection management.
        Add setSelection() to establish a selection comprised of contiguous,
        expanded sentences.
    
        This will selection will then be used to add selection controls and convert
        a selection into a note.
    
    public/js/view/paragraph.js
    public/js/view/sentence.js
        Make use of the click helper.
    
    public/js/jquery.summary-08.js
    public/js/model/comment.js
    public/js/model/doc.js
    public/js/model/note.js
    public/js/model/paragraph.js
    public/js/model/position.js
    public/js/model/section.js
    public/js/model/sentence.js
    public/js/model/user.js
    public/js/view/doc.js
    public/js/view/paragraph.js
    public/js/view/section.js
    public/js/view/sentence.js
        Adjust the structure for 'app' to include 'Controller' and 'Helper'.
    
    single-08-a.html
    public/js/view/paragraph.js
    public/js/view/section.js
    public/js/view/sentence.js
        Adjust the templates/views to include an explicit content container.
    
    public/css/summary-08.less
        Make p, .overlay, .controls, and .sentence unselectable.
        Make .expanded/.expansion selectable.
    
    public/js/rangy-1.2/core.js
        Convert to unix line endings.
    
    public/js/summary-08-full.js
    public/js/summary-08-full.min.js
        Re-pack

---
+++ public/js/helper/click.js
@@ -0,0 +1,93 @@
+/** @file
+ *
+ *  Backbone Helper to properly handle click events.
+ *
+ *  Requires:
+ *      backbone.js
+ *      jquery.js
+ */
+/*jslint nomen:false,laxbreak:true,white:false,onevar:false */
+/*global Backbone:false */
+(function() {
+    var app             = this.app = (this.app || {Model:{},      View:{},
+                                                   Controller:{}, Helper:{}});
+    var $               = jQuery.noConflict();
+    app.Helper.click    = {
+        clickEvent:         'element:click',
+        dblClickTimeout:    150,
+        events:             {
+            /* Track mouse events to fuse a single click event iff the mouse
+             * was IN this paragraph on mousedown AND mouseup/click.
+             */
+            'mousedown':    '_trackClick',
+            'click':        '_trackClick',
+            'mouseenter':   '_trackClick',
+            'mouseleave':   '_trackClick',
+            'dblclick':     '_trackClick'
+        },
+
+        /** @brief  Monitor mouseDown/Up for clicks WITHIN this element. */
+        _trackClick: function(e) {
+            var self    = this;
+
+            //console.log('Helper::_trackClick: type[ '+ e.type +' ]');
+
+            /* To ensure that nested elements perform properly, invoking a
+             * click event ONLY on the lowest level element, stop
+             * propagation of this mouse event.
+             */
+            e.stopPropagation();
+
+            switch (e.type)
+            {
+            case 'mousedown':
+                self._clickDown = e;
+                break;
+
+            case 'click':
+                if (self._clickDown !== null)
+                {
+                    /* We've seen a mousedown WITHIN this paragraph.  If this
+                     * 'up' event is NEAR the 'down' event, it is a potential
+                     * click.
+                     */
+                    var delta   = {
+                        x:  Math.abs( self._clickDown.pageX - e.pageX ),
+                        y:  Math.abs( self._clickDown.pageY - e.pageY )
+                    };
+
+                    if ((delta.x < 10) && (delta.y < 10))
+                    {
+                        /* In order to avoid squelching double-clicks, wait a
+                         * short time to see if there is an additional 'down'
+                         * event.
+                         */
+                        var orig    = self._clickDown;
+                        setTimeout(function() {
+                            if (orig === self._clickDown)
+                            {
+                                /*
+                                console.log('Helper::_trackClick: trigger[ '
+                                             + self.clickEvent +' ]');
+                                // */
+                                self.$el.trigger( self.clickEvent );
+                            }
+
+                            self._clickDown = null;
+                        }, self.dblClickTimeout);
+                        return;
+                    }
+                }
+
+                // Fallthrough to reset the click state
+
+            case 'dblclick':
+            case 'mouseenter':
+            case 'mouseleave':
+                self._clickDown = null;
+                break;
+            }
+        }
+    };
+
+ }).call(this);
